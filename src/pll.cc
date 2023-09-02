f65d998a875c0181ec18d82d2910e7b168699a9c
Chris G <chris@saturn.earth>
2023-09-02 15:17:49 -0500

    This is good for now.  On noisey signals and wideband FM, it's blah.  I'll tune it.

---
+++ src/pll.cc
@@ -0,0 +1,172 @@
+//*************************************************************************
+// File name: pll.cc
+//*************************************************************************
+
+//*************************************************************************
+// This program provides the ability to phase-lock and frequency-lock to
+// IQ (In-phase or Quadrature) signals that are provided by stdin.  The
+// data is 8-bit signed 2's complement, and is formatted as
+// I1,Q1; I2,Q2; ...
+// The phase-derotated samples will be written to stdout.
+//
+// To run this program type,
+// 
+//     ./pll > -d <displaytype> -r <sampleRate> -D
+//
+// where,
+//
+//    sampleRate - The sample rate of the IQ data in S/s.
+///*************************************************************************
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "PhaseLockedLoop.h"
+
+// This structure is used to consolidate user parameters.
+struct MyParameters
+{
+  float *sampleRatePtr;
+};
+
+/*****************************************************************************
+
+  Name: getUserArguments
+
+  Purpose: The purpose of this function is to retrieve the user arguments
+  that were passed to the program.  Any arguments that are specified are
+  set to reasonable default values.
+
+  Calling Sequence: exitProgram = getUserArguments(parameters)
+
+  Inputs:
+
+    parameters - A structure that contains pointers to the user parameters.
+
+  Outputs:
+
+    exitProgram - A flag that indicates whether or not the program should
+    be exited.  A value of true indicates to exit the program, and a value
+    of false indicates that the program should not be exited..
+
+*****************************************************************************/
+bool getUserArguments(int argc,char **argv,struct MyParameters parameters)
+{
+  bool exitProgram;
+  bool done;
+  int opt;
+
+  // Default not to exit program.
+  exitProgram = false;
+
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  // Default parameters.
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  // Default to 256000S/s.
+  *parameters.sampleRatePtr = 256000;
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+  // Set up for loop entry.
+  done = false;
+
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  // Retrieve the command line arguments.
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  while (!done)
+  {
+    // Retrieve the next option.
+    opt = getopt(argc,argv,"d:-r:-Dh");
+
+    switch (opt)
+    {
+      case 'r':
+      {
+        *parameters.sampleRatePtr = atof(optarg);
+        break;
+      } // case
+
+      case 'h':
+      {
+        // Display usage.
+        fprintf(stderr,"./pll -r samplerate (S/s)\n");
+
+        // Indicate that program must be exited.
+        exitProgram = true;
+        break;
+      } // case
+
+      case -1:
+      {
+        // All options consumed, so bail out.
+        done = true;
+      } // case
+    } // switch
+
+  } // while
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+  return (exitProgram);
+
+} // getUserArguments
+
+//*************************************************************************
+// Mainline code.
+//*************************************************************************
+int main(int argc,char **argv)
+{
+  bool done;
+  bool exitProgram;
+  uint32_t count;
+  int8_t inputBuffer[16384];
+  float sampleRate;
+  PhaseLockedLoop *pllPtr;
+  struct MyParameters parameters;
+
+   //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  // Default parameters.
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+  parameters.sampleRatePtr = &sampleRate;
+  //_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/_/
+
+  // Retrieve the system parameters.
+  exitProgram = getUserArguments(argc,argv,parameters);
+
+  if (exitProgram)
+  {
+    // Bail out.
+    return (0);
+  } // if
+
+  // Create a phase-locked loop.
+  pllPtr = new PhaseLockedLoop(sampleRate);
+
+  // Set up for loop entry.
+  done = false;
+
+  while (!done)
+  {
+    // Read a block of input samples (2 * complex FFT length).
+    count = fread(inputBuffer,sizeof(int8_t),16384,stdin);
+
+    if (count == 0)
+    {
+      // We're done.
+      done = true;
+    } // if
+    else
+    {
+      // Run the phase-locked loop.
+      pllPtr->run(inputBuffer,16384);
+
+      // Write the phase-derotated data to stdout.
+      fwrite(inputBuffer,sizeof(int8_t),count,stdout);
+    } // else
+  } // while
+
+  // Release resources.
+  delete pllPtr;
+
+  return (0);
+
+} // main
